678e9f03be89f64face8d75086b76d7ac2775a5e
muirurii <pnkanuna@gmail.com>
2022-08-28 12:01:10 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const introTextCont = document.querySelector(".intro");
+const cta = document.querySelector('.cta');
+const introText = `This world is but a, canvas to our, imagination.`;
+
+const withSpan = [...introText].map((letter, index) => {
+    return `<span class='opacity-0 ${letter === " " || letter === "," ? "" : "inline-block"} transform -translate-x-2 -translate-y-6' style='animation: text 400ms linear forwards; animation-delay:${
+    index * 40
+  }ms;'>${letter === "," ? "</br>" : letter === "" ? "&nbsp;" : letter}</span>`;
+});
+
+cta.style.animationDelay = `${withSpan.length * 40 + 400}ms`;
+
+setTimeout(() => {
+    introTextCont.innerHTML = withSpan.join("");
+}, 400);
+
+// const header = document.querySelector('header');
+
+// window.addEventListener("scroll", () => {
+
+//     // console.log(header.querySelector('::after'))
+//     const scroll = Math.round(window.scrollY / window.innerHeight * 100)
+//     header.setAttribute('data-scroll', `
+//             $ { scroll }
+//             px `)
+// });
+
+//Gallery
+
+const imgView = document.querySelector('.img-view');
+const activeImage = imgView.querySelector("img");
+const gallery = document.querySelector('.works');
+const zoomOut = imgView.querySelector(".zoom-out");
+const categories = document.querySelectorAll(".category");
+const images = gallery.querySelectorAll(".img");
+
+gallery.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("zoom-in")) return;
+    const selected = e.target.previousElementSibling.src;
+    activeImage.src = selected;
+    // imgView.classList.toggle("show-big-img");
+    imgView.classList.add("show-big-img");
+});
+
+zoomOut.addEventListener("click", () => {
+    imgView.classList.remove("show-big-img");
+    // imgView.classList.add("hidden");
+});
+
+categories.forEach(button => {
+    button.addEventListener("click", (e) => {
+        document.querySelector(".active-cat").classList.remove("active-cat");
+        e.target.classList.add("active-cat");
+        const activeCat = e.target.getAttribute("data-cat");
+        if (activeCat === "1") {
+            images.forEach(img => {
+                img.classList.add("block")
+                img.classList.remove("hidden")
+            });
+            return;
+        };
+        const active = gallery.querySelectorAll(`.img.${activeCat}`);
+        active.forEach(img => {
+            img.classList.add("block")
+            img.classList.remove("hidden")
+        });
+        const inactive = gallery.querySelectorAll(`.img:not(.${activeCat})`);
+        inactive.forEach(img => {
+            img.classList.remove("block")
+            img.classList.add("hidden")
+        });
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ["Plus Jakarta Sans", "sans-serif"],
+                hero: ["Bebas Neue", "sans-serif"]
+            },
+            width: {
+                header: "calc(100vw - 20%)"
+            },
+            colors: {
+                fade: "#ffffffb4"
+            }
+        },
+    },
+    plugins: [],
+}
